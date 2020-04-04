a730ed52084d7035e21ea59bae447a7d056fbb89
Austin Goh <zhaoyang@AAKUL-ICTMAC4108.local>
2020-04-04 20:02:19 +0800

    Using react-native-ble-peripheral to broadcast a service

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react'
 import 'react-native-gesture-handler'
 import { Provider } from 'react-redux'
 import { PersistGate } from 'redux-persist/integration/react'
-
+import BLEPeripheral from "react-native-ble-peripheral";
 import { Navigator } from '@routes'
 import { store, persistor } from '@store'
 
 const App = () => {
+  // const serviceUUID = "7a1781c4-c373-44aa-8cd6-c0f7bc1c0673";
+  // BLEPeripheral.addService(serviceUUID, true);
   return (
     <Provider store={store}>
       <PersistGate persistor={persistor}>
